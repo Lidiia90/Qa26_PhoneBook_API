beb2b2ee85fee5e2c3e8b8c00e21aafac8e02c6f
Lidiia90 <liduska200412@gmail.com>
2024-09-03 15:55:56 +0300

    tests RA

--- src/test/java/restassured/LoginTestsRA.java
+++ src/test/java/restassured/LoginTestsRA.java
@@ -1,11 +1,18 @@
 package restassured;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
+
 import dto.AuthRequestDTO;
 import dto.AuthResponseDTO;
+import dto.ErrorDTO;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class LoginTestsRA {
@@ -14,17 +21,14 @@ public class LoginTestsRA {
 
 
     @BeforeMethod
-    public void preCondition(){
+    public void preCondition() {
         RestAssured.baseURI = "https://contactapp-telran-backend.herokuapp.com";
         RestAssured.basePath = "v1";
     }
 
     @Test
-    public void loginSuccess(){
-        AuthRequestDTO authRequestDTO = AuthRequestDTO.builder()
-                .username("kate24@gmail.com")
-                .password("kaT45#kit")
-                .build();
+    public void loginSuccess() {
+        AuthRequestDTO authRequestDTO = AuthRequestDTO.builder().username("kate24@gmail.com").password("kaT45#kit").build();
 
         AuthResponseDTO responseDTO = given()
                 .body(authRequestDTO)
@@ -37,6 +41,40 @@ public class LoginTestsRA {
                 .response()
                 .as(AuthResponseDTO.class);
         System.out.println(responseDTO.getToken());
+
+    }
+
+    @Test
+    public void loginWrongEmail() {
+        AuthRequestDTO authRequestDTO = AuthRequestDTO.builder().username("maragmail.com").password("Mmar123456$").build();
+
+        ErrorDTO errorDTO =  given()
+                .body(authRequestDTO)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(endpoint)
+                .then()
+                .assertThat().statusCode(401)
+                .extract()
+                .response()
+                .as(ErrorDTO.class);
+        Assert.assertEquals(errorDTO.getMessage(),"Login or Password incorrect");
+    }
+
+    @Test
+    public void loginWrongEmailFormat() {
+        AuthRequestDTO authRequestDTO = AuthRequestDTO.builder().username("maragmail.com").password("Mmar123456$").build();
+
+        given()
+                .body(authRequestDTO)
+                .contentType(ContentType.JSON)
+                .when()
+                .post(endpoint)
+                .then()
+                .assertThat().statusCode(401)
+                .assertThat().body("message",containsString("Login or Password incorrect"))
+                .assertThat().body("path",equalTo("/v1/user/login/usernamepassword"));
+
     }
 
 }
